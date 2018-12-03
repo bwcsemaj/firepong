128c595a840acc2bf7f3ddfa6df9af1d9d9bc8b7
Leighzer <bwc_nothgiel@live.com>
2018-12-02 20:23:57 -0500

    Test commit

--- core/src/com/firepong/FirePong.java
+++ core/src/com/firepong/FirePong.java
@@ -21,7 +21,7 @@ public class FirePong extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		//batch.draw(img, 0, 0);
+		//grehagaergeragreabatch.draw(img, 0, 0);
 		batch.end();
 	}
 	
