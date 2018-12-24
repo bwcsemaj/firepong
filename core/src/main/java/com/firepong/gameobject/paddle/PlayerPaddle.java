154bdac3016005642f8f400e818879fbcb46157d
bwcsemaj <bwc_semaj@live.com>
2018-12-23 18:19:32 -0600

    test23

--- core/src/main/java/com/firepong/gameobject/paddle/PlayerPaddle.java
+++ core/src/main/java/com/firepong/gameobject/paddle/PlayerPaddle.java
@@ -1,5 +1,25 @@
 package com.firepong.gameobject.paddle;
 
-public class PlayerPaddle{
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.World;
+import com.firepong.game.CardinalDirection;
+import com.firepong.game.WorldController;
 
+public class PlayerPaddle extends Paddle{
+
+	// Attributes
+
+	// Start Constructors
+	public PlayerPaddle(World world, CardinalDirection cardinalDirection, Texture texture, int positionX,
+		int positionY){
+		super(world, cardinalDirection, texture, positionX, positionY);
+	}
+	// End Constructors
+
+	// Start Methods
+	@Override
+	public void move(WorldController controller){
+
+	}
+	// End Methods
 }
